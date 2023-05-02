e53a2c1c37e87bfd3e4c4f20d5eff8f785da9a5f
Joe Roddy <mrjoeroddy@gmail.com>
2023-05-02 18:06:54 -0400

    fix ntlfy w/ prisma

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { useAsync } from './hooks/useAsync';
+
 function App() {
-  return <div>Hello world</div>;
+  const [data] = useAsync(() => fetch(`/.netlify/functions/hello-world`).then((r) => r.json()));
+
+  return (
+    <div>
+      <pre>{JSON.stringify(data, null, 1)}</pre>
+    </div>
+  );
 }
 
 export default App;
