168266603e3269c2cf63cc410f35247c7130108a
Joe Roddy <mrjoeroddy@gmail.com>
2023-05-02 20:01:20 -0400

    clean up apis

--- api/bridg.ts
+++ api/bridg.ts
@@ -4,11 +4,8 @@ import { handleRequest } from 'bridg/app/server/request-handler';
 
 const prisma = new PrismaClient();
 
-export const handler: Handler = async (event, context) => {
-  const body = JSON.parse(event.body || '{}');
-  if (!body.model) return { statusCode: 200, body: JSON.stringify({ a: 1 }) };
-
+export const handler: Handler = async (event) => {
+  const body = JSON.parse(event.body as string);
   const { data, status } = await handleRequest(body, { db: prisma, rules: { default: true } });
-
   return { statusCode: status, body: JSON.stringify(data) };
 };
